f876fc01b7fa518ad3ceaa17bb1396a1787d5c93
Ray Ozzie <ray@ozzie.net>
2019-06-11 16:30:32 -0700

    touch

--- note.h
+++ note.h
@@ -1,4 +1,4 @@
-// Copyright 2018 Inca Roads LLC.  All rights reserved.
+// Copyright 2018 Inca Roads LLC.  All rights reserved. 
 // Use of this source code is governed by licenses granted by the
 // copyright holder including that found in the LICENSE file.
 
