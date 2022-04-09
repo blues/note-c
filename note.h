39183f011f0fd85391c97cc3b8061a4e163bd5ab
Ray Ozzie <ray@ozzie.net>
2022-04-08 21:28:11 -0400

    add unsigned template values, plus one helper

--- note.h
+++ note.h
@@ -101,6 +101,7 @@ bool NoteReset(void);
 void NoteResetRequired(void);
 #define NoteNewBody JCreateObject
 #define NoteAddBodyToObject(a, b) JAddItemToObject(a, "body", b)
+#define NoteGetBody(a) JGetObject(a, "body")
 J *NoteNewRequest(const char *request);
 J *NoteNewCommand(const char *request);
 J *NoteRequestResponse(J *req);
@@ -289,10 +290,15 @@ bool NotePayloadGetSegment(NotePayloadDesc *desc, const char segtype[NP_SEGTYPE_
 #define TINT24          13                  // 3-byte signed integer
 #define TINT32          14                  // 4-byte signed integer
 #define TINT64          18                  // 8-byte signed integer (note-c support depends upon platform)
+#define TUINT8          21                  // 1-byte unsigned integer (requires notecard firmware >= build 14444)
+#define TUINT16         22                  // 2-byte unsigned integer (requires notecard firmware >= build 14444)
+#define TUINT24         23                  // 3-byte unsigned integer (requires notecard firmware >= build 14444)
+#define TUINT32         24                  // 4-byte unsigned integer (requires notecard firmware >= build 14444)
 #define TFLOAT16        12.1                // 2-byte IEEE 754 floating point
 #define TFLOAT32        14.1                // 4-byte IEEE 754 floating point (a.k.a. "float")
 #define TFLOAT64        18.1                // 8-byte IEEE 754 floating point (a.k.a. "double")
 #define TSTRING(N)      _tstring(N)         // UTF-8 text of N bytes maximum (fixed-length reserved buffer)
+#define TSTRINGV        _tstring(0)         // variable-length string
 bool NoteTemplate(const char *notefileID, J *templateBody);
 
 // End of C-callable functions
