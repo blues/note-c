f4f900450e21b1ea6d68737747be2293cfb70722
Hayden Roche <hroche@blues.com>
2023-04-11 08:09:50 -0700

    Namespace test macros with NOTE_C_ prefix. (#64)

--- note.h
+++ note.h
@@ -73,15 +73,6 @@
 // originates from the Notecard, which synchronizes the time from both the cell network and GPS.
 typedef unsigned long int JTIME;
 
-// If we're building the tests, STATIC is defined to nothing. This allows the
-// tests to access the static functions in note-c. Among other things, this
-// let's us mock these normally static functions.
-#ifdef TEST
-#define STATIC
-#else
-#define STATIC static
-#endif
-
 // C-callable functions
 #ifdef __cplusplus
 extern "C" {
@@ -363,12 +354,6 @@ bool NotePayloadGetSegment(NotePayloadDesc *desc, const char segtype[NP_SEGTYPE_
 #define TSTRING(N)      _tstring(N)         // UTF-8 text of N bytes maximum (fixed-length reserved buffer)
 #define TSTRINGV        _tstring(0)         // variable-length string
 bool NoteTemplate(const char *notefileID, J *templateBody);
-
-// Make these normally static functions externally visible if building tests.
-#ifdef TEST
-bool timerExpiredSecs(uint32_t *timer, uint32_t periodSecs);
-void setTime(JTIME seconds);
-#endif
 
 // End of C-callable functions
 #ifdef __cplusplus
