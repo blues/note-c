8859d5f12d2e8081c6a064b9b8582dcc4ec6ec54
Zachary J. Fields <zachary_fields@yahoo.com>
2023-06-13 20:38:27 -0500

    chore: add static modifiers

--- n_request.c
+++ n_request.c
@@ -32,9 +32,9 @@ static uint16_t lastRequestSeqno = 0;
 #define	CRC_FIELD_NAME_OFFSET	1
 #define	CRC_FIELD_NAME_TEST		"\"crc\":\""
 #define REQUEST_RETRIES_ALLOWED 10
-int32_t crc32(const void* data, size_t length);
-char *crcAdd(char *json, uint16_t seqno);
-bool crcError(char *json, uint16_t shouldBeSeqno);
+NOTE_C_STATIC int32_t crc32(const void* data, size_t length);
+NOTE_C_STATIC char *crcAdd(char *json, uint16_t seqno);
+NOTE_C_STATIC bool crcError(char *json, uint16_t shouldBeSeqno);
 #endif
 
 /**************************************************************************/
@@ -639,7 +639,7 @@ static uint32_t lut[16] = {
     0x00000000,0x1DB71064,0x3B6E20C8,0x26D930AC,0x76DC4190,0x6B6B51F4,0x4DB26158,0x5005713C,
     0xEDB88320,0xF00F9344,0xD6D6A3E8,0xCB61B38C,0x9B64C2B0,0x86D3D2D4,0xA00AE278,0xBDBDF21C
 };
-int32_t crc32(const void* data, size_t length)
+NOTE_C_STATIC int32_t crc32(const void* data, size_t length)
 {
     uint32_t previousCrc32 = 0;
     uint32_t crc = ~previousCrc32;
