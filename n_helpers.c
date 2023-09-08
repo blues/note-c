993c1f85a5efa8912c7e3e894d1a039246f42617
Zachary J. Fields <zachary_fields@yahoo.com>
2023-09-08 20:43:32 +0000

    fix: astyle

--- n_helpers.c
+++ n_helpers.c
@@ -156,7 +156,7 @@ uint32_t NoteBinaryCodecEncode(const uint8_t *decData, uint32_t decDataLen,
         NOTE_C_LOG_ERROR(ERRSTR("NULL parameter", c_err));
         result = 0;
     } else if ((encBufSize < cobsEncodedMaxLength(decDataLen))
-           && (encBufSize < cobsEncodedLength(decData, decDataLen))) {
+               && (encBufSize < cobsEncodedLength(decData, decDataLen))) {
         // NOTE: `cobsEncodedMaxLength()` provides a constant time [O(1)] means
         //       of checking the buffer size. Only when it fails will the linear
         //       time [O(n)] check, `cobsEncodedLength()`, be invoked.
