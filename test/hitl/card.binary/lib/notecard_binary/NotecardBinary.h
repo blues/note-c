cd61a08adfcce8c8463bb5e61e44ef168707c74f
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-09-08 15:39:48 -0700

    feat: add defaults so that platformio.ini doesn't have to be continually switched between a local dev build and CI build.

--- test/hitl/card.binary/lib/notecard_binary/NotecardBinary.h
+++ test/hitl/card.binary/lib/notecard_binary/NotecardBinary.h
@@ -318,7 +318,11 @@ public:
     {
         J *req = notecard.newRequest("hub.set");
 
-        // This is the unique Product Identifier for your device
+#if !defined(PRODUCT_UID) && defined(DEFAULT_PRODUCT_UID)
+    #define PRODUCT_UID DEFAULT_PRODUCT_UID
+#endif
+
+// This is the unique Product Identifier for your device
 #ifndef PRODUCT_UID
 #define PRODUCT_UID "" // "com.my-company.my-name:my-project"
 #pragma message "PRODUCT_UID is not defined in this example. Please ensure your Notecard has a product identifier set before running this example or define it in code here. More details at https://dev.blues.io/tools-and-sdks/samples/product-uid"
