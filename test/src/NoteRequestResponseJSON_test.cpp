90e62b0b3edd4fd87408531698eb0ed1ba63c3da
Hayden Roche <hroche@blues.com>
2023-10-23 10:36:53 -0700

    Adjust/add unit tests to catch up to latest changes.

--- test/src/NoteRequestResponseJSON_test.cpp
+++ test/src/NoteRequestResponseJSON_test.cpp
@@ -59,7 +59,12 @@ SCENARIO("NoteRequestResponseJSON")
     }
 
     GIVEN("The request is a command pipeline") {
-        char req[] = "{\"cmd\":\"card.version\"}\n{\"cmd\":\"card.sleep\"}\n";
+        char cmd1[] = "{\"cmd\":\"card.version\"}\n";
+        char cmd2[] = "{\"cmd\":\"card.sleep\"}\n";
+        char req[sizeof(cmd1) + sizeof(cmd2) - 1] = {0};
+        strlcat(req, cmd1, sizeof(req));
+        strlcat(req, cmd2, sizeof(req));
+
         const size_t command_count = 2;
 
         WHEN("NoteRequestResponseJSON is called") {
@@ -68,10 +73,21 @@ SCENARIO("NoteRequestResponseJSON")
             THEN("NoteJSONTransaction is called once for each command") {
                 CHECK(NoteJSONTransaction_fake.call_count == command_count);
             }
+
+            THEN("NoteJSONTransaction is called with the correct pointer and "
+                 "length for each command") {
+                CHECK(NoteJSONTransaction_fake.arg0_history[0] == req);
+                CHECK(NoteJSONTransaction_fake.arg1_history[0] ==
+                      (sizeof(cmd1) - 1));
+                CHECK(NoteJSONTransaction_fake.arg0_history[1] ==
+                      (req + sizeof(cmd1) - 1));
+                CHECK(NoteJSONTransaction_fake.arg1_history[1] ==
+                      (sizeof(cmd2) - 1));
+            }
         }
     }
 
-    GIVEN("The request is not a command)") {
+    GIVEN("The request is not a command") {
         char req[] = "{\"req\":\"card.version\"}\n";
 
         WHEN("NoteRequestResponseJSON is called") {
@@ -81,6 +97,39 @@ SCENARIO("NoteRequestResponseJSON")
                  "response parameter") {
                 REQUIRE(NoteJSONTransaction_fake.call_count > 0);
                 CHECK(NoteJSONTransaction_fake.arg2_history[0] != NULL);
+            }
+        }
+    }
+
+    GIVEN("The request is not a command but it has an inner object with a "
+          "\"cmd\" field") {
+        char req[] = "{\"req\":\"note.add\",\"body\":{\"cmd\":\"fire\"}}\n";
+
+        WHEN("NoteRequestResponseJSON is called") {
+            char *rsp = NoteRequestResponseJSON(req);
+
+            THEN("The request is still treated as a regular request and not a "
+                 "command") {
+                REQUIRE(NoteJSONTransaction_fake.call_count > 0);
+                // The response parameter is non-NULL, meaning we expect a
+                // response (i.e. it's not a command).
+                CHECK(NoteJSONTransaction_fake.arg2_history[0] != NULL);
+            }
+        }
+    }
+
+    GIVEN("The request doesn't have a terminating newline") {
+        char req[] = "{\"req\":\"card.version\"}";
+
+        WHEN("NoteRequestResponseJSON is called") {
+            char *rsp = NoteRequestResponseJSON(req);
+
+            THEN("NoteJSONTransaction isn't called") {
+                CHECK(NoteJSONTransaction_fake.call_count == 0);
+            }
+
+            THEN("NULL is returned") {
+                CHECK(rsp == NULL);
             }
         }
     }
