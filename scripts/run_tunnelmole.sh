1c4dd2071d42e46e249fe3f2ab88c5ac2452068b
Hayden Roche <hroche@blues.com>
2023-12-06 14:15:49 -0800

    Fix tunnelmole step in HIL tests.
    Prior to this commit, tunnelmole was launched in the background and the GitHub
    job would proceed once the tmole.log file existed. The problem is that the next
    step depends on the environment variable MD5SRV_URL being set, and tmole.log
    gets created before that gets set. The result is that the next step may run
    prematurely, before MD5SRV_URL is set, causing that step to fail. The solution
    is to use a "ready" file. The tunnelmole step doesn't proceed until this file
    is created. run_tunnelmole.sh only creates this file once it has done
    everything it needs to, which includes setting MD5SRV_URL.

--- scripts/run_tunnelmole.sh
+++ scripts/run_tunnelmole.sh
@@ -18,5 +18,8 @@ MD5SRV_URL=`echo "$output" | grep https | cut -d " " -f1`
 echo "MD5SRV_URL=$MD5SRV_URL"
 [ -n "$MD5SRV_URL" ] || (echo "Could not fetch tunnelmole URL" && exit 1)
 echo "MD5SRV_URL=$MD5SRV_URL" >> $GITHUB_ENV
+# Only create tmole_ready once MD5SRV_URL has been set, as the next step in the
+# GitHub workflow depends on it.
+touch tmole_ready
 sleep 2
 echo "Tunnel mole started. Writing logs to `realpath tmole.log`"
