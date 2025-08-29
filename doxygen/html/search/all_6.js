4147c5be6c735983f94f5bc58868b52c3b636ffc
zfields <zfields@users.noreply.github.com>
2025-08-29 22:25:58 +0000

    deploy: d1b47cdd66e87bbe775611b00dec87919322f561

---
+++ doxygen/html/search/all_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['format_0',['format',['../structprintbuffer.html#a38337d25a56957deebce187d9ab089ef',1,'printbuffer']]],
+  ['free_5ffn_1',['free_fn',['../struct_j_hooks.html#a154d673fa4669abf51f7848c2c299718',1,'JHooks']]],
+  ['freefn_2',['freeFn',['../note_8h.html#a570f0444ac7f08f8d66bd12cf49b662c',1,'note.h']]]
+];
