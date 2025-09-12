c6aea4b6081bf3c819ae57187a18aeb9ecb7a15f
zfields <zfields@users.noreply.github.com>
2025-09-12 18:54:21 +0000

    deploy: 12db4f106367cbde57363c082480a51ad941aba1

---
+++ doxygen/html/search/all_12.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['valueint_0',['valueint',['../struct_j.html#a55e3d5d8de0cc6aff4d92c1b1228f7ea',1,'J']]],
+  ['valuenumber_1',['valuenumber',['../struct_j.html#ac62cf00a5cadeafc84d8d9fec38cfa50',1,'J']]],
+  ['valuestring_2',['valuestring',['../struct_j.html#a54f8d507791671d5f5951e8cae540232',1,'J']]]
+];
